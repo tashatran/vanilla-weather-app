b6cd9b28bbd1618e3c5b907c666d1514ed6c0846
Tasha <trn.tasha@gmail.com>
2021-10-26 13:44:39 -0500

    remove console log

--- src/app.js
+++ src/app.js
@@ -15,7 +15,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let cityElement = document.querySelector("#city");
