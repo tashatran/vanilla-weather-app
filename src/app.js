5d952121c72cabf52139a27da034ed1ccf6109fe
Tasha <trn.tasha@gmail.com>
2021-10-26 21:20:22 -0500

    added search engine

--- src/app.js
+++ src/app.js
@@ -35,8 +35,17 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "25fad9f7e87157d33dde0f82ab269ee8";
-let city = "San Francisco";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city} &appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "25fad9f7e87157d33dde0f82ab269ee8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city} &appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+search("San Francisco");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
